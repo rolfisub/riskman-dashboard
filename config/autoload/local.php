0903bc34c482ad13a542c46bd26c6792326aadff
Rolf Bansbach <rolfisub@gmail.com>
2017-07-23 23:46:39 -0400

    RIS-55 fix for composer and localphp

---
+++ config/autoload/local.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'db' => array(
+        'adapters' => array(
+            'db1' => array(
+                'database' => 'riskman',
+                'driver' => 'Mysqli',
+                'hostname' => '192.168.1.53',
+                'username' => 'riskman',
+                'password' => 'lXytl3kAvDCXMzFg',
+                'port' => '3306',
+            ), 
+        ),
+    ),
+);
