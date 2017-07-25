0c3256f5d6d934aaba8df2fb4cef139236ad20b2
Rolf Bansbach <rolf@softecdigital.com>
2017-07-25 01:34:52 -0400

    RIS-55 removing unnecessary controllers

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -10,41 +10,6 @@ return array(
     ),
     'router' => array(
         'routes' => array(
-            'Home' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route'    => '/Admin/home',
-                    'defaults' => array(
-                        'controller' => 'Admin\Controller\IndexController',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-            'Login' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route'    => '/Auth',
-                    'defaults' => array(
-                        'controller' => 'Admin\Controller\AuthController',
-                        'action'     => 'login',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'process' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:action]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
             'LoginRest' => array(
                 'type'    => 'Segment',
                 'options' => array(
