cc0a5e7391e2d67d95578299842f238346c5eaee
Rolf Bansbach <rolfisub@gmail.com>
2017-08-08 02:28:31 -0400

    RIS-8 subtask RIS-70 backend get all books

--- module/Admin/src/Admin/Controller/BooksRestController.php
+++ module/Admin/src/Admin/Controller/BooksRestController.php
@@ -24,7 +24,7 @@ class BooksRestController extends ProtectedRestfulController
      */
     public function getList() 
     {
-        return new JsonModel();
+        return new JsonModel($this->model->getBooks());
     }
     
     public function get($username)
--- module/Admin/src/Admin/Entity/Book.php
+++ module/Admin/src/Admin/Entity/Book.php
@@ -28,23 +28,8 @@ class Book extends AbstractEntity
     public function __construct($data) 
     {
         $this->setCreateReqFields([
-            'username',
-            'password',
-            'email',
-            'firstname',
-            'lastname'
-        ])->setCreateOptFields([
-        ])->setUpdateReqFields([
-            'username'
-        ])
-        ->setUpdateOptFields([
-            'password',
-            'passwordnew',
-            'passwordnew2',            
-            'email',
-            'firstname',
-            'lastname'
-        ]);
+            'name'
+        ])->setCreateOptFields([])->setUpdateReqFields([])->setUpdateOptFields([]);
         parent::__construct($data);
     }
     
@@ -52,69 +37,6 @@ class Book extends AbstractEntity
     {
         //check structure integrity
         $this->isUpdateStructValid();
-        
-        //username
-        $strlen = new StringLength(['min'=>'4', 'max'=>'32']);
-        if(!$strlen->isValid($this->data['username'])){
-            throw new Error400('username needs to be between 4 and 32 in length');
-        }
-        $alnum = new Alnum();
-        if(!$alnum->isValid($this->data['username'])){
-            throw new Error400('username can only contain alpha numeric characters ?');
-        }
-        
-        //email optional
-        if(isset($this->data['email'])) {
-            $email = new EmailAddress();
-            if (!$email->isValid($this->data['email'])) {
-                throw new Error400($email->getMessages());
-            }
-        }
-        
-        $namesLen = new StringLength(['min'=> '1', 'max' => '64']);
-        //firstname
-        if(isset($this->data['firstname'])) {
-            if(!$namesLen->isValid($this->data['firstname'])) {
-                throw new Error400('firstname needs to be between 1 and 64 in length');
-            }
-        }
-        
-        //lastname
-        if(isset($this->data['lastname'])) {
-            if(!$namesLen->isValid($this->data['lastname'])) {
-                throw new Error400('lastname needs to be between 1 and 64 in length');
-            }
-        }
-        
-        //password
-        if(isset($this->data['password']) && !empty($this->data['password'])) {
-            //check for minimum
-            $password = new Password();
-            if(!$password->isValid($this->data['password'])) {
-                throw new Error400($password->getMessages());
-            }
-            
-            //hash password
-            //$this->data['password'] = $this->getHash($this->data['password']);
-            
-        }
-        
-        //passwordnew
-        if(isset($this->data['passwordnew']) && !empty($this->data['passwordnew'])) {
-            //check for minimum
-            $password = new Password();
-            if(!$password->isValid($this->data['passwordnew'])) {
-                throw new Error400($password->getMessages());
-            }
-            
-            //check that password is set
-            if(!isset($this->data['password']) || empty($this->data['password'])) {
-                throw new Error400('Please provide current password to update it.');
-            }
-            
-            //hash password
-            //$this->data['passwordnew'] = $this->getHash($this->data['passwordnew']);
-        }
         
         return true;
     }
@@ -126,44 +48,6 @@ class Book extends AbstractEntity
     {
         //validate structure
         $this->isCreateStructValid();
-        
-        //username
-        //alphanumeric
-        //min 4 max 32
-        $strlen = new StringLength(['min'=>'4', 'max'=>'32']);
-        if(!$strlen->isValid($this->data['username'])){
-            throw new Error400('username needs to be between 4 and 32 in length');
-        }
-        $alnum = new Alnum();
-        if(!$alnum->isValid($this->data['username'])){
-            throw new Error400('username can only contain alpha numeric characters ?');
-        }
-        //password
-        $password = new Password();
-        if(!$password->isValid($this->data['password'])) {
-            throw new Error400($password->getMessages());
-        }
-        
-        //email
-        $email = new EmailAddress();
-        if (!$email->isValid($this->data['email'])) {
-            throw new Error400($email->getMessages());
-        }
-        
-        //firstname & lastname
-        $namesLen = new StringLength(['min'=> '1', 'max' => '64']);
-        if(!$namesLen->isValid($this->data['firstname'])) {
-            throw new Error400('firstname needs to be between 1 and 64 in length');
-        }
-        if(!$namesLen->isValid($this->data['lastname'])) {
-            throw new Error400('lastname needs to be between 1 and 64 in length');
-        }
-        /**
-         * once everything is ready we hash the password
-         */
-        
-        //create password hash and save it
-        $this->data['password'] = $this->getHash($this->data['password']);         
         
         return true;
         
