8610852dd05f6d737ae4468afe86c3c52dce9f78
Rolf Bansbach <rolf@softecdigital.com>
2017-10-19 23:15:30 -0400

    RIS-127 dev extreme trial

--- public/admin/master/components/riskman/pages/book.options/options.ranking.controller.js
+++ public/admin/master/components/riskman/pages/book.options/options.ranking.controller.js
@@ -5,9 +5,9 @@
         .module('riskman.book.options')
         .controller('OptionsRankingController', OptionsRankingController);
 
-    OptionsRankingController.$inject = ['$scope', '$stateParams', 'bookFormat', '$timeout'];
+    OptionsRankingController.$inject = ['$scope', '$stateParams', 'bookRanking', '$timeout', '$q'];
 
-    function OptionsRankingController($scope, $stateParams, bookFormat, $timeout) {
+    function OptionsRankingController($scope, $stateParams, bookRanking, $timeout, $q) {
         var c = this;
         
         /*
@@ -22,34 +22,80 @@
          * bookRanking model
          */
         c.bookRanking = [
-            {
-                rank:'A',
-                max_line_change_percent:'10',
-                max_amount_expected:'100',
-                limit_percent:'1000',
-                limit_amount:'10000'
-            },
-            {
-                rank:'B',
-                max_line_change_percent:'10',
-                max_amount_expected:'100',
-                limit_percent:'1000',
-                limit_amount:'10000'
-            },
-            {
-                rank:'C',
-                max_line_change_percent:'10',
-                max_amount_expected:'100',
-                limit_percent:'1000',
-                limit_amount:'10000'
-            },
         ];
+        
+        /**
+         * set value bookranking model
+         */
+        c.setRankingValue = function(updateKey, updateValue) {
+            angular.forEach(c.bookRanking, function(value, key){
+                if(updateKey === value) {
+                    angular.merge(c.bookRanking[key], updateValue);
+                    return;
+                }
+            }, this);
+            console.log("Updating Controller Model");
+            console.log(c.bookRanking);
+        };
         
         /**
          * grid options
          */
         c.gridOptions = {
-            dataSource: c.bookRanking,
+            dataSource: /*c.bookRanking,*/
+            {
+                load: function(){
+                    console.log("Data Source Load");
+                    var def = $q.defer();
+                    var r = bookRanking.getBookRanking(c.myBook.id);
+                    r.then(function(res){
+                        def.resolve(res.data.bookRanking.rankings);
+                        angular.merge(c.bookRanking, res.data.bookRanking.rankings);
+                    }, bookRanking.onError);
+                    return def.promise;
+                },
+                update: function(key, value){
+                    console.log("Data Source Update");
+                    c.setRankingValue(key, value);
+                    var def = $q.defer();
+                    var data = {
+                        rankings: JSON.stringify(c.bookRanking) 
+                    };
+                    var r = bookRanking.updateBookRanking(c.myBook.id, data);
+                    r.then(function(res){
+                        def.resolve(c.bookRanking);
+                    }, bookRanking.onError)
+                    return def.promise;
+                }
+            },
+            onEditingStart: function(e) {
+                console.log("EditingStart");
+                console.log(e);
+                
+            },
+            onInitNewRow: function(e) {
+                console.log("InitNewRow");
+            },
+            onRowInserting: function(e) {
+                console.log("RowInserting");
+            },
+            onRowInserted: function(e) {
+                console.log("RowInserted");
+            },
+            onRowUpdating: function(e) {
+                console.log("RowUpdating");
+                console.log(e);
+            },
+            onRowUpdated: function(e) {
+                console.log("RowUpdated");
+                console.log(e);
+            },
+            onRowRemoving: function(e) {
+                console.log("RowRemoving");
+            },
+            onRowRemoved: function(e) {
+                console.log("RowRemoved");
+            },
             allowColumnReordering: false,
             allowColumnResizing: false,
             columnAutoWidth: true,
@@ -60,12 +106,49 @@
                 enabled: false
             },
             editing:{
-                mode: 'cell',
+                mode: 'batch',
                 allowUpdating: true,
                 allowDeleting: false,
-                allowAdding: false,
+                allowAdding: false
             },
-            columns:['rank', 'max_line_change_percent','max_amount_expected', 'limit_percent', 'limit_amount']
+            columns:[
+                {
+                    caption: 'Rank',
+                    alignment: 'center',
+                    cellTemplate: 'rank',
+                    dataField: 'rank'
+                },
+                {
+                    caption: 'Max % Line Move',
+                    alignment: 'center',
+                    cellTemplate: 'max_line_change_percent',
+                    dataField: 'max_line_change_percent'
+                },
+                {
+                    caption: 'Max Expected',
+                    alignment: 'center',
+                    cellTemplate: 'max_amount_expected',
+                    dataField: 'max_amount_expected'
+                },
+                {
+                    caption: 'Limit %',
+                    alignment: 'center',
+                    cellTemplate: 'limit_percent',
+                    dataField: 'limit_percent'
+                },
+                {
+                    caption: 'Limit',
+                    alignment: 'center',
+                    cellTemplate: 'limit_amount',
+                    dataField: 'limit_amount'
+                },
+                
+//                'rank', 
+//                'max_line_change_percent',
+//                'max_amount_expected', 
+//                'limit_percent', 
+//                'limit_amount'
+            ]
         };
         
         /**
@@ -97,7 +180,10 @@
          * @returns {undefined}
          */
         c.init = function() {
-            
+            var r = bookRanking.getBookRanking(c.myBook.id);
+            r.then(function(res){
+                angular.merge(c.bookRanking, res.data.bookRanking.rankings);
+            }, bookRanking.onError);
         };
         
         
