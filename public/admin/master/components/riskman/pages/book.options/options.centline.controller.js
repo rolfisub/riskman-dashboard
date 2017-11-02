28ee918ac4c5cae82bf87148632fb101212ffa72
Rolf Bansbach <rolf@softecdigital.com>
2017-11-02 00:05:53 -0400

    RIS-128 subtask RIS-148 frontend validation

--- public/admin/master/components/riskman/pages/book.options/options.centline.controller.js
+++ public/admin/master/components/riskman/pages/book.options/options.centline.controller.js
@@ -10,6 +10,16 @@
     function OptionsCentlineController($scope, $stateParams, bookCentline, $timeout, $q) {
         var c = this;
         
+        
+        c.touched = false;
+        
+        /**
+         * set input as touched
+         */
+        c.touch = function() {
+            c.touched = true;
+        };
+        
         /*
          * model for book
          */
@@ -35,12 +45,88 @@
         c.loadPresetClick = function(){
             if(c.bookCentline.presetSelected !== '') {
                 var key = c.bookCentline.presetSelected;
-                c.bookCentline.current = c.bookCentline.presets[key];
+                var preset = JSON.parse(JSON.stringify(c.bookCentline.presets[key]));
+                c.bookCentline.current = preset;
             } else {
                 //please select a preset to load
                 console.log("please select a preset to load");
             }
         };
+        
+        /**
+         * save current centline model
+         * @returns {undefined}
+         */
+        c.saveBookCentline = function() {
+            if(c.bookCentline.current.length > 0) {
+                var data = {
+                    centline: JSON.stringify(c.bookCentline.current)
+                };
+                var r = bookCentline.updateBookCentline(c.myBook.id, data);
+                r.then(function(res){
+                    showSuccessFor(3);
+                    c.init();
+                }, bookCentline.onError);
+            }
+        };
+        
+        /*
+         * checks if the ml provided is ok
+         */
+        c.checkMLInput = function(ml, pos) {
+            if(pos === 0) {
+                if(ml !== -101) {
+                    return 'ML can only be -101 at this position.';
+                }
+            } else {
+                var topMl = c.bookCentline.current[pos -1].ml;
+                if(pos + 1 === c.bookCentline.current.length) {
+                    if(ml >= topMl) {
+                        return 'ML needs to be less than ' + topMl;
+                    }
+                } else {
+                    var bottomMl = c.bookCentline.current[pos +1].ml;
+                    if(ml >= topMl) {
+                        return 'ML needs to be less than ' + topMl;
+                    }
+                    if(ml <= bottomMl) {
+                        return 'ML needs to be greater than ' + bottomMl;
+                    }
+                }
+            }
+        };
+        
+        /*
+         * checks if cent value is valid
+         */
+        c.checkCentInput = function(cent, pos){
+            console.log(cent);console.log(pos);
+            //check for greater than 5 and less than next pos
+            if(cent < 5) {
+                return 'Cent cannot be less than 5';
+            }
+            var bottomCent = c.bookCentline.current[pos +1].cent;
+            if(pos === 0) {
+                if(cent >= bottomCent) {
+                    return 'Cent needs to be less than ' + bottomCent;
+                }
+            } else {
+                var topCent = c.bookCentline.current[pos -1].cent;
+                if(pos + 1 === c.bookCentline.current.length) {
+                    if(cent <= topCent) {
+                        return 'Cent needs to be greater than ' + topCent;
+                    }
+                } else {
+                    if(cent <= topCent) {
+                        return 'Cent needs to be greater than ' + topCent;
+                    }
+                    if(cent >= bottomCent) {
+                        return 'Cent needs to be less than ' + bottomCent;
+                    }
+                }
+            }
+        };
+        
         
         
         
@@ -51,7 +137,7 @@
         c.init = function() {
             var r = bookCentline.getBookCentline(c.myBook.id);
             r.then(function(res){
-                console.log(res);
+                c.bookCentline.current = res.data.bookCentline.centline;
             }, bookCentline.onError);
             
             /**
@@ -60,6 +146,7 @@
             var p = bookCentline.getBookCentlinePresets();
             p.then(function(res){
                 c.bookCentline.presets = res.data.centlinePresets;
+                c.touched = false;
             }, bookCentline.onError);
         };
         
