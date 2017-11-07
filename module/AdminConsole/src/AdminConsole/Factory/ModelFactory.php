9c77b36fefab5cdd4796047759b421a3ed95f923
Rolf Bansbach <rolf@softecdigital.com>
2017-11-07 02:06:57 -0500

    RIS-163 subtask RIS-168 create console foundation partial commit 1

---
+++ module/AdminConsole/src/AdminConsole/Factory/ModelFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AdminConsole\Factory;
+
+/**
+ * Description of ModelFactory
+ *
+ * @author rolf
+ */
+class ModelFactory {
+    //put your code here
+}
